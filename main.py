3e01854e3ec6c2e4da3574fcb6f9a3e5f0e3eb61
Phonktezy <144049246+Phonktezy@users.noreply.github.com>
2023-10-12 12:10:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# def distance(x1, y1 ,x2, y2):
+#     res = ((x2-x1)**2 + (y2-y1)**2) ** 0.5
+#     return res
+#
+#
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# print(distance(x1, y1, x2, y2))
+
+
+
+
+
+
